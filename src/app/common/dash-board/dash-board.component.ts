f9bec7b4dedd86c02b1eeab1d3286dbb77a77bf7
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2024-11-22 08:53:04 +0530

    new commit

--- src/app/common/dash-board/dash-board.component.ts
+++ src/app/common/dash-board/dash-board.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dash-board',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule,FormsModule,HttpClientModule],
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
 export class DashBoardComponent {
+
+  constructor(private router: Router) {}
+
   totalBooks = 150;
   booksBorrowed = 40;
   booksAvailable = 110;
@@ -18,5 +25,26 @@ export class DashBoardComponent {
       'Jane Smith returned "To Kill a Mockingbird"',
       'Alice Brown borrowed "1984"',
   ];
+
+
+  public onAddBooksClick(){
+    this.router.navigate(['/AddBooks']);
+  }
+
+  public onManageBooksClick(){
+    this.router.navigate(['/ManageBooks']);
+  }
+  public onAddMembersClick(){
+    this.router.navigate(['/AddMembers']);
+  }
+  public onManageMembersClick(){
+    this.router.navigate(['/ManageMembers']);
+  }
+  public onAddBorrowersClick(){
+    this.router.navigate(['/AddBorrowers']);
+  }
+  public onManageBorrowersClick(){
+    this.router.navigate(['/ManageBorrowers']);
+  }
 
 }
